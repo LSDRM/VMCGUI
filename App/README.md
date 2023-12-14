b938ef6ede1593404f469e2951e96de64c5e071b
B137P107 <90220128+B137P107@users.noreply.github.com>
2023-12-14 20:37:40 +0100

    Delete App directory

--- App/README.md
+++
@@ -1,7 +0,0 @@
-Installation
-==========
-
-1. Install python
-2. #Install ftd2xx driver  # Those are only for kits using FTDI chips.
-3. #Install pip ftd2xx ... #
-4. Blablabla
--- App/_globals.py
+++
@@ -1,7 +0,0 @@
-def init():
-    global url, firstExec, DataWindow, dataPlotID, currentKit
-    url = ''
-    firstExec = True
-    DataWindow = []
-    dataPlotID = 0
-    currentKit = ''
